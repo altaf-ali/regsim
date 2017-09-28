4cd283fa64e03c43ebadcfc56ccc84c8fa08e1b8
Altaf Ali <altaf@fastmail.com>
2017-09-28 13:45:24 +0100

    simplified the regular expression using named classes and custom pseudo-grammar

--- R/formula_terms.R
+++ R/formula_terms.R
@@ -9,9 +9,27 @@ formula_terms.formula <- function(formula) {
   term_labels <- labels(stats::terms(formula))
 
   # this captures any transformation functions (log, lag, as.factor, poly, etc)
-  # test regex at https://regex101.com/r/tknU6o/3
+  # test regex at https://regex101.com/r/tknU6o/4
 
-  regex_expr <- "^([a-zA-Z][a-zA-Z0-9_\\.]*)(?:\\s*(?:[+-:\\*]\\s*[a-zA-Z0-9]*|\\(\\s*([a-zA-Z0-9_\\.]*)(?:\\s*(?:[\\^\\*+-]\\s*[a-zA-Z0-9_\\.]*|,\\s*[a-zA-Z0-9_\\.]*\\s*)*)\\s*\\))){0,1}\\s*$"
+  # define our own classes to simplify the regular expresions for parsing
+  regex_classes <- c(
+    id     = '[[:alpha:]][[:alnum:]_\\\\.]*', # an identifier
+    op     = '[+\\\\-\\\\/\\\\*\\\\:^%]',     # an operator
+    num    = '[[:digit:]\\\\.]*',             # a number
+    punc   = ',',                             # punctuations
+    lparen = '\\\\(',                         # left paren
+    rparen = '\\\\)'                          # right paren
+  )
+
+  regex_classes <- sapply(regex_classes, paste0, "[[:space:]]*")
+  regex_classes <- stats::setNames(regex_classes,
+                                   paste0("<", names(regex_classes), ">"))
+
+  regex_expr <- "^(<id>)(?:(?:<op><id>)*|<lparen>(<id>|<num>)(?:(?:<punc>|<op>)(?:<id>|<num>))*<rparen>){0,1}$"
+  regex_expr <- stringr::str_replace_all(regex_expr, regex_classes)
+
+  # for debugging print out the regex and test on regex101
+  # cat(paste0("\n", regex_expr, "\n\n"))
 
   regex_matches <- regmatches(term_labels, regexec(regex_expr, term_labels))
 
